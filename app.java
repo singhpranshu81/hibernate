598b59ad8c11b23931518b87aab85f6fd2cee9f7
singhpranshu81 <thescholar02@gmail.com>
2024-09-06 16:50:53 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,12 @@
+package com.mph.view;
+
+import com.mph.controller.EmployeeController;
+
+public class App {
+
+	public static void main(String[] args) {
+		EmployeeController ec=new EmployeeController();
+		ec.addEmployee();
+	}
+
+}
