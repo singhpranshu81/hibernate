68a3840468c5750aa9c9d42a9e19e5e0a34d5ae6
Pranshu Singh <thescholar02@gmail.com>
2024-09-09 11:56:13 +0530

    Update employee.java

--- employee.java
+++ employee.java
@@ -1,18 +1,31 @@
 package com.mph.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="emply")
+@NamedQueries({
+	@NamedQuery(name="GET_EMP_BY_ID",query="from Emplyee e where e.eid= :id"),
+	@NamedQuery(name="GET_EMP_BY_DID",query="from Emplyee e where e.did= :ed")
+	
+	
+})
 public class Emplyee {
 	@Id
 	private int eid;
-	private String ename;
+	@Embedded
+	private Name ename;
 	private String email;
 	private String password;
 	private String gender;
@@ -21,9 +34,21 @@ public class Emplyee {
 	@OneToOne
 	@JoinColumn(name="DeptID")
 	private Department did;
+	
+	@OneToMany(mappedBy = "emp", cascade = CascadeType.ALL )
+	private List<Address> address;
 	public Emplyee() {
 		super();
 	}
+	
+	public List<Address> getAddress() {
+		return address;
+	}
+
+	public void setAddress(List<Address> address) {
+		this.address = address;
+	}
+
 	public Department getDid() {
 		return did;
 	}
@@ -36,12 +61,15 @@ public class Emplyee {
 	public void setEid(int eid) {
 		this.eid = eid;
 	}
-	public String getEname() {
+	
+	public Name getEname() {
 		return ename;
 	}
-	public void setEname(String ename) {
+
+	public void setEname(Name ename) {
 		this.ename = ename;
 	}
+
 	public String getEmail() {
 		return email;
 	}
