66e422bd47173173692c8a1958ad5d96ddbc7984
Pranshu Singh <thescholar02@gmail.com>
2024-09-09 11:55:50 +0530

    Update EmployeeController.java

--- EmployeeController.java
+++ EmployeeController.java
@@ -1,15 +1,23 @@
 package com.mph.controller;
 
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
+import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
 
+import com.mph.entity.Address;
 import com.mph.entity.Department;
 import com.mph.entity.Emplyee;
+import com.mph.entity.Name;
 
 public class EmployeeController {
   SessionFactory sf=new Configuration().configure().buildSessionFactory();
@@ -17,6 +25,7 @@ public class EmployeeController {
   Transaction txn;
   Emplyee emp;
   Department d;
+  Address ad;
   Scanner sc=new Scanner(System.in);
   public void addEmployee() {
 	  txn=session.beginTransaction();
@@ -27,9 +36,12 @@ public class EmployeeController {
 	  System.out.println("enter email: ");
 	  String e= sc.next();
 	  emp.setEmail(e);
-	  System.out.println("enter name: ");
-	  String name= sc.next();
-	  emp.setEname(name);
+	  System.out.println("enter first name: ");
+	  String fname= sc.next();
+	  System.out.println("enter last name: ");
+	  String lname= sc.next();
+	  Name ename = new Name(fname,lname);
+	  emp.setEname(ename);
 	    System.out.println("enter gender: ");
 	  String gender= sc.next();
 	  emp.setGender(gender);
@@ -49,9 +61,105 @@ public class EmployeeController {
 	  
 	  emp.setDid(d);//setting object department to employee
 	  
+	  ad=new Address();
+	  
+	  System.out.println("enter Address id: ");
+	  ad.setAddrId(sc.nextLong());
+	  System.out.println("enter Door no: ");
+	  ad.setDoorNo(sc.nextInt());
+	  System.out.println("enter Street: ");
+	  ad.setStreet(sc.next());
+	  System.out.println("enter city: ");
+	  ad.setCity(sc.next());
+	  System.out.println("enter pincode: ");
+	  ad.setPincode(sc.nextInt());
+	  List<Address> al=new ArrayList<Address>();
+	  ad.setEmp(emp);
+	  al.add(ad);
+	  
+	  emp.setAddress(al);
+	  session.save(ad);
 	  session.save(emp);
 	  session.save(d);
 	  txn.commit();
 	  System.out.println("Employee added successfully!!!");
   }
+  public void viewEmployee() {
+	  session.clear();
+	  Query qry= session.createQuery("from Emplyee");
+	  List<Emplyee> l=qry.list();
+	  Iterator i=l.iterator();
+	  while(i.hasNext()) {
+		  System.out.println(i.next());
+	  }
+  }
+  
+  public void updateEmployee() {
+	  txn=session.beginTransaction();
+	  
+	  System.out.println("Enter your eid: ");
+	  int p=sc.nextInt();
+	  System.out.println("Enter email to be updated: ");
+	  String l=sc.next();
+	  
+	  Query qry=session.createQuery("update Emplyee set email =:mail where eid =:j").setString("mail", l).setInteger("j",p);
+	  int row=qry.executeUpdate();
+	  txn.commit();
+	  System.out.println("updated "+row+" successfully");
+  }
+  
+  
+  public void getEmployeeID() {
+	  System.out.println("Enter your eid: ");
+	  int emid=sc.nextInt();
+	  Query qry=session.createNamedQuery("GET_EMP_BY_ID").setInteger("id", emid);
+	  Emplyee emp= (Emplyee) qry.uniqueResult();
+	  System.out.println(emp);
+  }
+  
+  public void searchByEmail() {
+	  System.out.println("Enter email : ");
+	  String l=sc.next();
+	  Criteria c=session.createCriteria(Emplyee.class);
+	  c.add(Restrictions.eqOrIsNull("email", l));
+	  Emplyee emp= (Emplyee) c.uniqueResult();
+	  System.out.println(emp);
+  }
+  
+  public void getEmployeeByDept() {
+	  List<Emplyee> e=new ArrayList<Emplyee>();
+	  System.out.println("Enter your dept name: ");
+	  String dept=sc.next();
+	  Query qry=session.createNamedQuery("GET_EMP_BY_Deptname").setString("d", dept);
+	  List<Emplyee> emp= ( List<Emplyee>) qry.getResultList();
+	  Iterator i=emp.iterator();
+	  while(i.hasNext()) {
+		  Department d=(Department) i.next();
+		  int did=d.getDid();
+		  Query y=session.createNamedQuery("GET_EMP_BY_DID").setInteger("ed", did);
+		  Emplyee f= (Emplyee) y.uniqueResult();
+		  e.add(f);
+	  }
+	  System.out.println(e);
+  }
+  
+  
+  public void nameStartWithM() {
+	  List<Emplyee> e=new ArrayList<Emplyee>();
+	  Criteria c=session.createCriteria(Emplyee.class);
+	  List<Emplyee> emp= c.add(Restrictions.like("ename.fname", "M%")).list();
+	  System.out.println(emp);
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 }
